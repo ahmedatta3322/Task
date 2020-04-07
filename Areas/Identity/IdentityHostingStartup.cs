4a9e84ca4a19102fcc63a8f7587826cd5eecaa55
ahmedatta3322 <30796979+ahmedatta3322@users.noreply.github.com>
2020-04-06 20:22:01 -0700

    Add files via upload

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using tasktp.Data;
+
+[assembly: HostingStartup(typeof(tasktp.Areas.Identity.IdentityHostingStartup))]
+namespace tasktp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
