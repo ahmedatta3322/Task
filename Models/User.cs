58ef1490675af46f512b1108fc8332d6c08c3625
ahmedatta3322 <30796979+ahmedatta3322@users.noreply.github.com>
2020-04-06 20:29:24 -0700

    Add files via upload

---
+++ Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace task.Models
+{
+    public class User
+    {
+        [Required]
+        public String Username { set; get; }
+
+        public int Id { get; set; }
+
+        public string City { get; set; }
+        public string Address { get; set; }
+
+        public string SpeakingLanguage { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime GradeYear { get; set; }
+        //public string Access { get; set; }
+        [DataType(DataType.Text)]
+        public string Comment { get; set; }
+    }
+}
